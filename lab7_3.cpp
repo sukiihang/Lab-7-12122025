6cbaeb500227dfe3d9a3c4c25c2c3fcc0b5972be
Sukrit <sukrit.cho@gmail.com>
2025-12-16 00:51:46 +0700

    lab7complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char x) {
+    if (x >= 'A' && x <= 'Z') {
+        if (x == 'A') {
+            return 'Z';
+        } else {
+            return x - 1;
+        }
+    } else {
+        return '0';
+    }
 }
 
 int main(){
